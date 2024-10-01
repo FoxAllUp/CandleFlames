fd1ac1c39b82eb32f665199420fe30eb239c956e
FoxAllUp <richardhanzel93@gmail.com>
2024-10-01 15:31:18 +0200

    Make a little cleaner the database

--- src/data/products-db.js
+++ src/data/products-db.js
@@ -1,4 +1,5 @@
 const products = [
+    //Cube Candles
     { id: 1, name: 'Cube Dark Blue Candle', shape: 'cube', color: 'dark_blue' },
     { id: 2, name: 'Cube Peach Candle', shape: 'cube', color: 'peach' },
     { id: 3, name: 'Cube Pink Candle', shape: 'cube', color: 'pink' },
@@ -6,12 +7,7 @@ const products = [
     // Cylindrical Candles
     { id: 4, name: 'Cylindrical Burgundy Candle', shape: 'cylindrical', color: 'burgundy' },
     { id: 5, name: 'Cylindrical Cognac Candle', shape: 'cylindrical', color: 'cognac' },
-    {
-      id: 6,
-      name: 'Cylindrical Creamy Peach Candle',
-      shape: 'cylindrical',
-      color: 'creamy_peach'
-    },
+    { id: 6, name: 'Cylindrical Creamy Peach Candle', shape: 'cylindrical', color: 'creamy_peach'},
     { id: 7, name: 'Cylindrical Ice Blue Candle', shape: 'cylindrical', color: 'ice_blue' },
     { id: 8, name: 'Cylindrical Light Blue Candle', shape: 'cylindrical', color: 'light_blue' },
     { id: 9, name: 'Cylindrical Mint Candle', shape: 'cylindrical', color: 'mint' },
@@ -19,12 +15,7 @@ const products = [
     { id: 11, name: 'Cylindrical Peach Candle', shape: 'cylindrical', color: 'peach' },
     { id: 12, name: 'Cylindrical Pink Candle', shape: 'cylindrical', color: 'pink' },
     { id: 13, name: 'Cylindrical Purple Candle', shape: 'cylindrical', color: 'purple' },
-    {
-      id: 14,
-      name: 'Cylindrical Red Special Candle',
-      shape: 'cylindrical',
-      color: 'red_special'
-    },
+    { id: 14, name: 'Cylindrical Red Special Candle', shape: 'cylindrical', color: 'red_special' },
     { id: 15, name: 'Cylindrical Red Candle', shape: 'cylindrical', color: 'red' },
     { id: 16, name: 'Cylindrical White Candle', shape: 'cylindrical', color: 'white' },
     { id: 17, name: 'Cylindrical Wine Candle', shape: 'cylindrical', color: 'wine' },
@@ -75,84 +66,29 @@ const products = [
     { id: 54, name: 'Square Prism Blue Candle', shape: 'square_prism', color: 'blue' },
     { id: 55, name: 'Square Prism Burgundy Candle', shape: 'square_prism', color: 'burgundy' },
     { id: 56, name: 'Square Prism Cognac Candle', shape: 'square_prism', color: 'cognac' },
-    {
-      id: 57,
-      name: 'Square Prism Dark Blue Candle',
-      shape: 'square_prism',
-      color: 'dark_blue'
-    },
+    { id: 57, name: 'Square Prism Dark Blue Candle', shape: 'square_prism', color: 'dark_blue' },
     { id: 58, name: 'Square Prism Green Candle', shape: 'square_prism', color: 'green' },
     { id: 59, name: 'Square Prism Orange Candle', shape: 'square_prism', color: 'orange' },
     { id: 60, name: 'Square Prism Peach Candle', shape: 'square_prism', color: 'peach' },
     { id: 61, name: 'Square Prism Pink Candle', shape: 'square_prism', color: 'pink' },
     { id: 62, name: 'Square Prism Purple Candle', shape: 'square_prism', color: 'purple' },
     { id: 63, name: 'Square Prism Red Candle', shape: 'square_prism', color: 'red' },
-    {
-      id: 64,
-      name: 'Square Prism Turquoise Candle',
-      shape: 'square_prism',
-      color: 'turquoise'
-    },
+    { id: 64, name: 'Square Prism Turquoise Candle', shape: 'square_prism', color: 'turquoise' },
     { id: 65, name: 'Square Prism White Candle', shape: 'square_prism', color: 'white' },
     { id: 66, name: 'Square Prism Yellow Candle', shape: 'square_prism', color: 'yellow' },
 
     // Triangular Prism Candles
     { id: 67, name: 'Triangular Prism Blue Candle', shape: 'triangular_prism', color: 'blue' },
-    {
-      id: 68,
-      name: 'Triangular Prism Burgundy Candle',
-      shape: 'triangular_prism',
-      color: 'burgundy'
-    },
-    {
-      id: 69,
-      name: 'Triangular Prism Cognac Candle',
-      shape: 'triangular_prism',
-      color: 'cognac'
-    },
-    {
-      id: 70,
-      name: 'Triangular Prism Green Candle',
-      shape: 'triangular_prism',
-      color: 'green'
-    },
-    {
-      id: 71,
-      name: 'Triangular Prism Orange Candle',
-      shape: 'triangular_prism',
-      color: 'orange'
-    },
-    {
-      id: 72,
-      name: 'Triangular Prism Peach Candle',
-      shape: 'triangular_prism',
-      color: 'peach'
-    },
-    {
-      id: 73,
-      name: 'Triangular Prism Purple Candle',
-      shape: 'triangular_prism',
-      color: 'purple'
-    },
+    { id: 68, name: 'Triangular Prism Burgundy Candle', shape: 'triangular_prism', color: 'burgundy' },
+    { id: 69, name: 'Triangular Prism Cognac Candle', shape: 'triangular_prism', color: 'cognac' },
+    { id: 70, name: 'Triangular Prism Green Candle', shape: 'triangular_prism', color: 'green' },
+    { id: 71, name: 'Triangular Prism Orange Candle', shape: 'triangular_prism', color: 'orange' },
+    { id: 72, name: 'Triangular Prism Peach Candle', shape: 'triangular_prism', color: 'peach' },
+    { id: 73, name: 'Triangular Prism Purple Candle', shape: 'triangular_prism', color: 'purple' },
     { id: 74, name: 'Triangular Prism Red Candle', shape: 'triangular_prism', color: 'red' },
-    {
-      id: 75,
-      name: 'Triangular Prism Turquoise Candle',
-      shape: 'triangular_prism',
-      color: 'turquoise'
-    },
-    {
-      id: 76,
-      name: 'Triangular Prism White Candle',
-      shape: 'triangular_prism',
-      color: 'white'
-    },
-    {
-      id: 77,
-      name: 'Triangular Prism Yellow Candle',
-      shape: 'triangular_prism',
-      color: 'yellow'
-    },
+    { id: 75, name: 'Triangular Prism Turquoise Candle', shape: 'triangular_prism', color: 'turquoise' },
+    { id: 76, name: 'Triangular Prism White Candle', shape: 'triangular_prism', color: 'white' },
+    { id: 77, name: 'Triangular Prism Yellow Candle', shape: 'triangular_prism', color: 'yellow' },
 
     // Square Prism Steps Candles
     { id: 78, name: 'Square Prism Steps Red Candle', shape: 'square_prism_steps', color: 'red' }
